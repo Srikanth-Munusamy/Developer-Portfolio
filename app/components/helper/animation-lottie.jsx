85e8948e460dd928453c1f2d1ccdfcd552315218
Srikanth-Munusamy <srikanth28092004@gmail.com>
2024-11-23 18:29:35 +0530

    changed lottie

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,30 @@
 "use client";
 
-import Lottie from "lottie-react";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+// Dynamically import the Lottie component with `ssr: false` to disable SSR
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+  const [defaultOptions, setDefaultOptions] = useState(null);
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  useEffect(() => {
+    if (animationPath) {
+      setDefaultOptions({
+        loop: true,
+        autoplay: true,
+        animationData: animationPath, // Ensure this is a valid JSON object
+        style: {
+          width: width || "95%",
+        },
+      });
+    }
+  }, [animationPath, width]);
+
+  if (!defaultOptions) return null; // Prevent rendering until options are set
+
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
